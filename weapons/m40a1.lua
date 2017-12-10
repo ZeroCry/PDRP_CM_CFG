924d03dbc1d3061e49b3fa3263251ccce96ed108
aidybee <aidybee@gmail.com>
2017-12-10 18:53:45 +1000

    Update m40a1.lua

--- weapons/m40a1.lua
+++ weapons/m40a1.lua
@@ -1,12 +1,12 @@
-WEAPON.NAME = "M40-A1"
+WEAPON.NAME = "The M40-A1 is a bolt-action sniper rifle"
 WEAPON.INFORMATION = "Ey Thats P Good"
 WEAPON.MODEL = "models/weapons/w_m40.mdl"
 WEAPON.SKIN = 0
 WEAPON.SWEP = "cw_poseidon_m40a1"
  
-WEAPON.SELL = 0
-WEAPON.BUY = 0
+WEAPON.SELL = 900
+WEAPON.BUY = 1000
  
-WEAPON.WEIGHT = 5
+WEAPON.WEIGHT = 7.5
  
 CRAFTINGMOD.WEAPONS:Register(WEAPON)

add71a157d9e12ffe84a2d0aac6c071629e12a94
aidybee <aidybee@gmail.com>
2017-12-10 19:00:13 +1000

    Update m5s90.lua

--- weapons/m5s90.lua
+++ weapons/m5s90.lua
@@ -1,12 +1,12 @@
-WEAPON.NAME = "M5S90"
+WEAPON.NAME = "The Mossberg 590 is a pump action shotgun."
 WEAPON.INFORMATION = "Ey Thats P Good"
 WEAPON.MODEL = "models/weapons/w_m590.mdl"
 WEAPON.SKIN = 0
 WEAPON.SWEP = "cw_poseidon_m590"
  
-WEAPON.SELL = 0
-WEAPON.BUY = 0
+WEAPON.SELL = 550
+WEAPON.BUY = 700
  
-WEAPON.WEIGHT = 5
+WEAPON.WEIGHT = 3.4
  
 CRAFTINGMOD.WEAPONS:Register(WEAPON)

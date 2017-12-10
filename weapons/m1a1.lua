04c6c86c774f5e7e6d2488d25385bf87b206caaf
aidybee <aidybee@gmail.com>
2017-12-10 19:20:37 +1000

    Update m1a1.lua

--- weapons/m1a1.lua
+++ weapons/m1a1.lua
@@ -1,12 +1,12 @@
 WEAPON.NAME = "M1A1"
-WEAPON.INFORMATION = "Ey Thats P Good"
+WEAPON.INFORMATION = "The M1A1 carbine is a semi-automatic stock variant of the M4 carbine."
 WEAPON.MODEL = "models/weapons/w_cw_kk_ins2_m1a1.mdl"
 WEAPON.SKIN = 0
 WEAPON.SWEP = "cw_poseidon_m1a1"
  
-WEAPON.SELL = 0
-WEAPON.BUY = 0
+WEAPON.SELL = 650
+WEAPON.BUY = 800
  
-WEAPON.WEIGHT = 5
+WEAPON.WEIGHT = 3.4
  
 CRAFTINGMOD.WEAPONS:Register(WEAPON)

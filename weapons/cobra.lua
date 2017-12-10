13f2f005335835f76744a9c18d2eefda6b3a9a81
aidybee <aidybee@gmail.com>
2017-12-10 18:32:03 +1000

    Update cobra.lua

--- weapons/cobra.lua
+++ weapons/cobra.lua
@@ -4,8 +4,8 @@ WEAPON.MODEL = "models/weapons/w_cw_kk_ins2_cstm_cobra.mdl"
 WEAPON.SKIN = 0
 WEAPON.SWEP = "cw_poseidon_cobra"
  
-WEAPON.SELL = 0
-WEAPON.BUY = 0
+WEAPON.SELL = 500
+WEAPON.BUY = 700
  
 WEAPON.WEIGHT = 5
  

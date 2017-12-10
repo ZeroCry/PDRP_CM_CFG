1c1838a0defe2c2c7728a51008442a94e1b6f1cb
aidybee <aidybee@gmail.com>
2017-12-10 18:56:30 +1000

    Update m4a1.lua

--- weapons/m4a1.lua
+++ weapons/m4a1.lua
@@ -1,12 +1,12 @@
 WEAPON.NAME = "M4A1"
-WEAPON.INFORMATION = "Ey Thats P Good"
+WEAPON.INFORMATION = "The M4A1 carbine is a fully automatic variant of the M4 carbine."
 WEAPON.MODEL = "models/weapons/w_m4a1.mdl"
 WEAPON.SKIN = 0
 WEAPON.SWEP = "cw_poseidon_m4a1"
  
-WEAPON.SELL = 0
-WEAPON.BUY = 0
+WEAPON.SELL = 550
+WEAPON.BUY = 700
  
-WEAPON.WEIGHT = 5
+WEAPON.WEIGHT = 3.4
  
 CRAFTINGMOD.WEAPONS:Register(WEAPON)

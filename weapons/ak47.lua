721e752f498b5c8d156f91866c38e8a339de68f5
aidybee <aidybee@gmail.com>
2017-12-10 18:42:30 +1000

    Update ak47.lua

--- weapons/ak47.lua
+++ weapons/ak47.lua
@@ -1,12 +1,12 @@
 WEAPON.NAME = "AK-47"
-WEAPON.INFORMATION = "Ey Thats P Good"
+WEAPON.INFORMATION = "The AK-47 is a full auto, gass operated assault rifle."
 WEAPON.MODEL = "models/weapons/w_ak74.mdl"
 WEAPON.SKIN = 0
 WEAPON.SWEP = "cw_poseidon_ak47"
  
-WEAPON.SELL = 0
-WEAPON.BUY = 0
+WEAPON.SELL = 600
+WEAPON.BUY = 800
  
-WEAPON.WEIGHT = 5
+WEAPON.WEIGHT = 3.1
  
 CRAFTINGMOD.WEAPONS:Register(WEAPON)

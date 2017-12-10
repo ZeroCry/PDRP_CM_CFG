4487b1912ae13ff71d9a1faf90004f18c3267139
aidybee <aidybee@gmail.com>
2017-12-10 19:42:09 +1000

    Update ump45.lua

--- weapons/ump45.lua
+++ weapons/ump45.lua
@@ -1,12 +1,12 @@
 WEAPON.NAME = "UMP45"
-WEAPON.INFORMATION = "Ey Thats P Good"
+WEAPON.INFORMATION = "The UMP45 is a magazine-fed submachine gun."
 WEAPON.MODEL = "models/weapons/w_ump45.mdl"
 WEAPON.SKIN = 0
 WEAPON.SWEP = "cw_poseidon_ump45"
  
-WEAPON.SELL = 0
-WEAPON.BUY = 0
+WEAPON.SELL = 475
+WEAPON.BUY = 600
  
-WEAPON.WEIGHT = 5
+WEAPON.WEIGHT = 3
  
 CRAFTINGMOD.WEAPONS:Register(WEAPON)

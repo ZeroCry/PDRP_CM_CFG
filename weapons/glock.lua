70aab29a2b28b72b8791789e2bd454287a4f3cff
aidybee <aidybee@gmail.com>
2017-12-10 19:39:01 +1000

    Update glock.lua

--- weapons/glock.lua
+++ weapons/glock.lua
@@ -1,12 +1,12 @@
 WEAPON.NAME = "Glock"
-WEAPON.INFORMATION = "Ey Thats P Good"
+WEAPON.INFORMATION = "The Glock is a short recoil-operated, locked-breech semi-automatic pistol."
 WEAPON.MODEL = "models/weapons/w_cw_kk_ins2_cstm_g19.mdl"
 WEAPON.SKIN = 0
 WEAPON.SWEP = "cw_poseidon_g19"
  
-WEAPON.SELL = 0
-WEAPON.BUY = 0
+WEAPON.SELL = 325
+WEAPON.BUY = 500
  
-WEAPON.WEIGHT = 5
+WEAPON.WEIGHT = 0.3
  
 CRAFTINGMOD.WEAPONS:Register(WEAPON)
